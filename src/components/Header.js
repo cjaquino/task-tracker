56c4fa978b46a70da41430833880ab83e52c3b70
Carl <jerritt.aquino@gmail.com>
2021-04-20 10:23:14 -0400

    add routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import Button from './Button'
+import { useLocation } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 const Header = ({ title, onAdd, showAdd }) => {
-  const onClick = () => {
-    console.log('in header')
-  }
+  const location = useLocation()
 
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color={showAdd ? 'Red' : 'Green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+      {location.pathname == '/' && (
+        <Button
+          color={showAdd ? 'Red' : 'Green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
     </header>
   )
 }
